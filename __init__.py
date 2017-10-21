875334bb4a81cd044a7ed7b9b627bfcd8a21061b
heshenghuan <heshenghuan@sina.com>
2017-10-21 20:07:46 +0800

    __init__ file

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 21 2017
+
+@author: Heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+"""
+
+from src import features, parameters, pretreatment, utils
+
+__version__ = 'NeuralTextProcess lib 0.1.3'
+
+__all__ = [
+    "features", "parameters",
+    "utils", "pretreatment"
+]
--- src/__init__.py
+++ src/__init__.py
@@ -6,10 +6,3 @@ Created on Sat Jul 08 2017
 @author: Heshenghuan (heshenghuan@sina.com)
 http://github.com/heshenghuan
 """
-
-__all__ = [
-    "features", "parameters",
-    "utils", "pretreatment"
-]
-
-__version__ = 'NeuralTextProcess lib 0.1.3'
